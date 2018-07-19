25fd933e987810a96a235256e7611a423bd1ddaa
redgoose <scripter@me.com>
2018-07-20 07:58:57 +0900

    다른 환경에서 cors 관련 문제가 일어나서 조정

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 		header("Access-Control-Allow-Credentials: true");
 		header('Access-Control-Allow-Headers: X-Requested-With');
 		header('Access-Control-Allow-Headers: Content-Type');
+		header('Access-Control-Allow-Headers: Authorization');
 		header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 		header('Access-Control-Max-Age: 86400');
 	}
