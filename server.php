c12da9f15dbcedbb31e434c12110c51a5c5210fd
redgoose-dev <scripter@me.com>
2018-10-11 08:48:18 +0900

    로컬서버 스크립트 조정
    로컬서버에서 이미지나 다른파일들을 access-control-allow-origin 풀기

---
+++ server.php
@@ -0,0 +1,18 @@
+<?php
+$extensions = ['php'];
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$ext = pathinfo($path, PATHINFO_EXTENSION);
+
+if (in_array($ext, $extensions) || !$ext)
+{
+	require_once 'index.php';
+}
+else
+{
+	$file = $_SERVER['SCRIPT_FILENAME'];
+	$type = mime_content_type($file);
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: '.$type);
+	header('Content-Length: '.filesize($file));
+	readfile($file);
+}
