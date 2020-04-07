f1d1cf2c9b6832e6e5309b77cdd855fe87e9d890
redgoose <scripter@me.com>
2020-04-08 01:21:34 +0900

    안쓰는 부분 삭제

--- cmd.sh
+++ cmd.sh
@@ -8,17 +8,6 @@ start() {
   php -S 0.0.0.0:$port server.php
 }
 
-# initial docker environment
-init-docker() {
-  mkdir data
-  mkdir data/upload
-  chmod 707 data
-  chmod 707 data/upload
-  curl -O https://raw.githubusercontent.com/redgoose-dev/goose-api/master/resource/.env.example
-  curl -O https://raw.githubusercontent.com/redgoose-dev/goose-api/master/docker-compose.yml
-  mv .env.example .env
-}
-
 case "$1" in
   start)
     start
@@ -40,12 +29,8 @@ case "$1" in
     php resource/tools.php reset-password
     ;;
 
-  init-docker)
-    init-docker
-    ;;
-
   *)
-    echo "Usage: ./dev-server.sh {start|ready|install|make-token|reset-password|init-docker}" >&2
+    echo "Usage: ./cmd.sh {start|ready|install|make-token|reset-password}" >&2
     exit 3
     ;;
 esac
