0da2543ffd608552e8c74f75672d671e06571085
redgoose <scripter@me.com>
2020-04-18 13:33:17 +0900

    app_name 값 안나오는 현상 수정

--- controller/nests/get_index.php
+++ controller/nests/get_index.php
@@ -15,38 +15,38 @@ try
   // connect db
   $this->model->connect();
 
-	// set where
-	$where = '';
-	if ($app = $_GET['app'])
-	{
-		$where .= ($app === 'null' || $app === 'NULL') ? ' and app_srl IS NULL' : ' and app_srl='.$app;
-	}
-	if ($id = $_GET['id'])
-	{
-		$where .= ' and id LIKE \''.$id.'\'';
-	}
-	if ($name = $_GET['name'])
-	{
-		$where .= ' and name LIKE \'%'.$name.'%\'';
-	}
+  // set where
+  $where = '';
+  if ($app = $_GET['app'])
+  {
+    $where .= ($app === 'null' || $app === 'NULL') ? ' and app_srl IS NULL' : ' and app_srl='.$app;
+  }
+  if ($id = $_GET['id'])
+  {
+    $where .= ' and id LIKE \''.$id.'\'';
+  }
+  if ($name = $_GET['name'])
+  {
+    $where .= ' and name LIKE \'%'.$name.'%\'';
+  }
   if ($user_srl = $_GET['user'])
   {
     $where .= ' and user_srl='.(int)$user_srl;
   }
 
-	// check access
-	$token = Controller\Main::checkAccessIndex($this->model, true);
-	$where .= (!$token->data->admin && $token->data->user_srl) ? ' and user_srl='.(int)$token->data->user_srl : '';
+  // check access
+  $token = Controller\Main::checkAccessIndex($this->model, true);
+  $where .= (!$token->data->admin && $token->data->user_srl) ? ' and user_srl='.(int)$token->data->user_srl : '';
 
-	// output
-	$output = Controller\Main::index((object)[
+  // output
+  $output = Controller\Main::index((object)[
     'model' => $this->model,
-		'table' => 'nests',
-		'where' => $where,
-		'json_field' => ['json'],
-	]);
+    'table' => 'nests',
+    'where' => $where,
+    'json_field' => ['json'],
+  ]);
 
-	// get articles count
+  // get articles count
   if ($output->data && Util::checkKeyInExtField('count_articles'))
   {
     $output->data->index = Controller\nests\UtilForNests::getCountArticles(
@@ -59,24 +59,24 @@ try
   // get app title
   if ($output->data && Util::checkKeyInExtField('app_name'))
   {
-    $foo = Controller\nests\UtilForNests::getAppName(
+    $output->data->index = Controller\nests\UtilForNests::getAppName(
       $this->model,
       $output->data->index,
       $token
     );
   }
 
-	// set token
-	if ($token) $output->_token = $token->jwt;
+  // set token
+  if ($token) $output->_token = $token->jwt;
 
   // disconnect db
   $this->model->disconnect();
 
-	// output
-	Output::data($output);
+  // output
+  Output::data($output);
 }
 catch (Exception $e)
 {
   $this->model->disconnect();
-	Error::data($e->getMessage(), $e->getCode());
+  Error::data($e->getMessage(), $e->getCode());
 }
