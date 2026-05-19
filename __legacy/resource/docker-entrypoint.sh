9a053707c336548dead618b47299b0afc2ec38db
GoOSe <scripter@me.com>
2026-05-19 13:35:26 +0900

    레거시 파일 삭제
    일단 ignore 로 변경

--- __legacy/resource/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# install
-python install.py -y
-
-# run server
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-80}
--- __legacy/src/libs/check.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def check_url(url: str) -> bool:
-    if url.startswith('http://') or url.startswith('https://'):
-        return True
-    else:
-        raise Exception('URL validation error.')
